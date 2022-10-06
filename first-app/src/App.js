55e7d5e7fe9e28d45bd9a25e9c13a887c2e39c41
pnichols789 <pnichols789@gmail.com>
2022-10-05 19:32:34 -0500

    -

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -3,9 +3,43 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello React.</h1>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
 
+function Header() {
+  return (
+    <div>Header</div>
+  )
+}
+
+function Body() {
+  return (
+    <div>
+      Body Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quas quo laboriosam id praesentium. Reiciendis at iusto autem quo quam repudiandae facere dolorum tenetur commodi nam? Voluptate, tempora excepturi! Dolor!
+    </div>
+  )
+}
+function Footer() {
+  return (
+    <div>
+      <ul>
+        <li>facebook</li>
+        <li>facebook</li>
+        <li>facebook</li>
+      </ul>
+    </div>
+  )
+}
+
+
+
+
+
+
+
 export default App;
+
