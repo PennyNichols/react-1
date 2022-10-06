f5bc34e21182c473b4a3b4d9555f0ad39a4b5746
pnichols789 <pnichols789@gmail.com>
2022-10-05 19:42:39 -0500

    -

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import React from "react"
 
+console.log(React, "<<<<")
 function App() {
   return (
     <div className="App">
@@ -20,6 +22,9 @@ function Body() {
   return (
     <div>
       Body Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quas quo laboriosam id praesentium. Reiciendis at iusto autem quo quam repudiandae facere dolorum tenetur commodi nam? Voluptate, tempora excepturi! Dolor!
+      <Image />
+      <Title />
+      <MainInfo />
     </div>
   )
 }
@@ -31,15 +36,48 @@ function Footer() {
         <li>facebook</li>
         <li>facebook</li>
       </ul>
+      <Address />
     </div>
   )
 }
 
 
+class Image extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src="https://miro.medium.com/max/1400/0*3C6CUn1FEC_raM8c" alt="" width="200" />
+      </div >
+    )
+  }
+}
 
+class Title extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to our first react class</h2>
+      </div >
+    )
+  }
+}
 
+class MainInfo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>React is fast js library</h2>
+      </div >
+    )
+  }
+}
 
-
+function Address(){
+  return(
+    <div>
+      <p>123 example dr</p>
+    </div>
+  )
+}
 
 export default App;
-
